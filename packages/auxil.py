ff99e924ecc594a871efedff8f38ef2681b08266
odermatt <dani.odermatt@gmail.com>
2019-02-11 12:20:49 +0100

    first ticino job fails, why??

--- packages/auxil.py
+++ packages/auxil.py
@@ -35,6 +35,7 @@ def  list_xml_scene_dir(scenesdir, sensor='OLCI', file_list=[]):
             if temp == '':
                 print('no xml found in ' + s)
             else:
+                print(temp)
                 xmlfs.append(temp[0])
     elif sensor.upper() == 'MSI':
         if not file_list:
