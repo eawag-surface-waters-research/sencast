aab0c315b718eebdd06813d991c35b1c146d5d18
odermatt <dani.odermatt@gmail.com>
2019-12-23 18:42:32 +0100

    some of the pixel coordinate extraction did not work with the new polymer call, fix attempt...

--- packages/product_fun.py
+++ packages/product_fun.py
@@ -23,53 +23,69 @@ def get_corner_pixels_ROI(product, params):
         ur = [max(lons), max(lats)]
         lr = [max(lons), min(lats)]
         ll = [min(lons), min(lats)]
-        
-        
-        ul_pos = product.getSceneGeoCoding().getPixelPos(GeoPos(ul[1], 
-                                                                ul[0]), None)
-        ur_pos = product.getSceneGeoCoding().getPixelPos(GeoPos(ur[1], 
-                                                                ur[0]), None)
-        lr_pos = product.getSceneGeoCoding().getPixelPos(GeoPos(lr[1], 
-                                                                lr[0]), None)
-        ll_pos = product.getSceneGeoCoding().getPixelPos(GeoPos(ll[1], 
-                                                                ll[0]), None)
+
+        ul_pos = product.getSceneGeoCoding().getPixelPos(GeoPos(ul[1], ul[0]), None)
+        ur_pos = product.getSceneGeoCoding().getPixelPos(GeoPos(ur[1], ur[0]), None)
+        lr_pos = product.getSceneGeoCoding().getPixelPos(GeoPos(lr[1], lr[0]), None)
+        ll_pos = product.getSceneGeoCoding().getPixelPos(GeoPos(ll[1], ll[0]), None)
         
         ul_bool = ul_pos.x > 0 and ul_pos.y > 0
         ur_bool = ur_pos.x > 0 and ur_pos.y > 0
         lr_bool = lr_pos.x > 0 and lr_pos.y > 0
         ll_bool = ll_pos.x > 0 and ll_pos.y > 0
         
-        UL = np.array([np.floor(ul_pos.getY()).astype(int), 
-                           np.floor(ul_pos.getX()).astype(int)])
-        UR = np.array([np.floor(ur_pos.getY()).astype(int), 
-                           np.floor(ur_pos.getX()).astype(int)])
-        LR = np.array([np.ceil(lr_pos.getY()).astype(int), 
-                       np.ceil(lr_pos.getX()).astype(int)])
-        LL = np.array([np.ceil(ll_pos.getY()).astype(int), 
-                       np.ceil(ll_pos.getX()).astype(int)])
-        
+        UL = np.array([np.floor(ul_pos.getY()).astype(int), np.floor(ul_pos.getX()).astype(int)])
+        UR = np.array([np.floor(ur_pos.getY()).astype(int), np.floor(ur_pos.getX()).astype(int)])
+        LR = np.array([np.ceil(lr_pos.getY()).astype(int), np.ceil(lr_pos.getX()).astype(int)])
+        LL = np.array([np.ceil(ll_pos.getY()).astype(int), np.ceil(ll_pos.getX()).astype(int)])
+
         h = product.getSceneRasterHeight()
         w = product.getSceneRasterWidth()
-        
-        # I see 8 configurations which can give errors
-        if not ul_bool and not ur_bool and lr_bool and not ll_bool:
-            UL = [0, 0]
-        if not ul_bool and ur_bool and lr_bool and not ll_bool:
-            UL = [UR[0], 0]
-        if not ul_bool and ur_bool and not lr_bool and not ll_bool:
-            UL = [UR[0],0]
-            LR = [h, UR[1]]
-        if ul_bool and ur_bool and not lr_bool and not ll_bool:
-            LR = [h, UR[1]]
-        if ul_bool and not ur_bool and not lr_bool and not ll_bool:
-            LR = [h, w]
-        if ul_bool and not ur_bool and not lr_bool and ll_bool:
-            LR = [LL[0], w]
-        if not ul_bool and not ur_bool and not lr_bool and ll_bool:
-            UL = [0, LL[1]]
-            LR = [LL[0], w]
-        if not ul_bool and not ur_bool and lr_bool and ll_bool:
-            UL = [0, LL[1]]
+
+        # missing with perimeter partly too north
+        if not ul_bool and not ur_bool:
+            # and too west (only LR covered)
+            if not ll_bool:
+                UL = [0, 0]
+                UR = [0, LR[1]]
+                LL = [LR[0], 0]
+            # and too east (only LL covered)
+            elif not lr_bool:
+                UL = [0, LL[1]]
+                UR = [0, w]
+                LR = [LL[0], w]
+            else:
+                UL = [0, LL[1]]
+                UR = [0, LR[1]]
+
+        # missing with perimeter partly too south
+        elif not ll_bool and not lr_bool:
+            # and too west (only UR covered)
+            if not ul_bool:
+                UL = [UR[0], 0]
+                LR = [h, UR[1]]
+                LL = [h, 0]
+            # and too east (only UL covered)
+            elif not ur_bool:
+                LL = [h, UL[1]]
+                UR = [UL[0], w]
+                LR = [h, w]
+            else:
+                LL = [h, UL[1]]
+                LR = [h, UR[1]]
+
+        # missing with perimeter partly too east
+        elif not ur_bool and not lr_bool:
+                UR = [UL[0], w]
+                LR = [LL[0], w]
+
+        # missing with perimeter partly too west
+        elif not ul_bool and not ll_bool:
+                UL = [UR[0], 0]
+                LL = [LR[0], 0]
+
+        # single missing corners are not necessary, right?
+
         return UL, UR, LR, LL
 
 
