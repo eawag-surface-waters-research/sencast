dcdc0bfe7fb501f7d1b33c58088c0a80a86da731
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-03-16 09:58:56 +0100

    reduced the number warnings

--- packages/main.py
+++ packages/main.py
@@ -34,12 +34,11 @@ def hindcast(params_filename):
         print('"output_data" directory not found in {} home folder'.format(path_config.user))
         return
     params['gpt_path'] = path_config.gpt_path
-    dir_dict = {}
 
     # *********************************************************
     # Initialisation of input folders
     # *********************************************************
-    dir_dict['L1 dir'] = os.path.join(l1_dir, params['sensor'].upper() + '_L1')
+    dir_dict = {'L1 dir': os.path.join(l1_dir, params['sensor'].upper() + '_L1')}
     if not os.path.isdir(dir_dict['L1 dir']):
         os.mkdir(dir_dict['L1 dir'])
     wktfn = os.path.basename(params['wkt file']).split('.')[0]
@@ -77,44 +76,47 @@ def hindcast(params_filename):
         dir_dict['proj dir'] = os.path.join(L2_dir_sensor, params['name'] + '_' + wktfn + '_' + params['start'][:10] + '_' + params['end'][:10])
         if not os.path.isdir(dir_dict['proj dir']):
             os.mkdir(dir_dict['proj dir'])
-        dir_dict['qlrgb dir'] = os.path.join(dir_dict['proj dir'],'L1P-rgb-quicklooks')
+        dir_dict['qlrgb dir'] = os.path.join(dir_dict['proj dir'], 'L1P-rgb-quicklooks')
         if not os.path.isdir(dir_dict['qlrgb dir']):
             os.mkdir(dir_dict['qlrgb dir'])
-        dir_dict['qlfc dir'] = os.path.join(dir_dict['proj dir'],'L1P-falsecolor-quicklooks')
+        dir_dict['qlfc dir'] = os.path.join(dir_dict['proj dir'], 'L1P-falsecolor-quicklooks')
         if not os.path.isdir(dir_dict['qlfc dir']):
             os.mkdir(dir_dict['qlfc dir'])
 
     # Idepix
-        dir_dict['L1P dir'] = os.path.join(dir_dict['proj dir'],'L1P_' +
-                                           os.path.basename(params['wkt file']).split('.')[0])
+        dir_dict['L1P dir'] = os.path.join(dir_dict['proj dir'], 'L1P_' +
+                                           os.path.basename(params['wkt file']).split('.')[0].decode())
         if not os.path.isdir(dir_dict['L1P dir']):
             os.mkdir(dir_dict['L1P dir'])
+
     # C2RCC
         if '1' in params['pcombo']:
             print('Creating C2RCC map directories')
             for c2rb in params['c2rcc bands']:
-                c2name = os.path.join(dir_dict['proj dir'],'L2C2R-' + c2rb)
+                c2name = os.path.join(dir_dict['proj dir'], 'L2C2R-' + c2rb)
                 if not os.path.isdir(c2name):
                     os.mkdir(c2name)
                 dir_dict[c2rb] = c2name
             print('Creating C2RCC L2 directory')
-            c2rcc_dir = os.path.join(dir_dict['proj dir'],'L2C2R')
+            c2rcc_dir = os.path.join(dir_dict['proj dir'], 'L2C2R')
             if not os.path.isdir(c2rcc_dir):
                 os.mkdir(c2rcc_dir)
             dir_dict['c2rcc dir'] = c2rcc_dir
+
     # Polymer
         if '2' in params['pcombo']:
             print('Creating Polymer map directories')
             for polyb in params['polymer bands']:
-                polyname = os.path.join(dir_dict['proj dir'],'L2POLY-' + polyb)
+                polyname = os.path.join(dir_dict['proj dir'], 'L2POLY-' + polyb)
                 if not os.path.isdir(polyname):
                     os.mkdir(polyname)
                 dir_dict[polyb] = polyname
             print('Creating Polymer L2 directory')
-            polymer_dir = os.path.join(dir_dict['proj dir'],'L2POLY')
+            polymer_dir = os.path.join(dir_dict['proj dir'], 'L2POLY')
             if not os.path.isdir(polymer_dir):
                 os.mkdir(polymer_dir)
             dir_dict['polymer dir'] = polymer_dir
+
     # MPH
         if '3' in params['pcombo'] and params['sensor'].upper() == 'OLCI':
             print('Creating MPH map directories')
@@ -124,7 +126,7 @@ def hindcast(params_filename):
                     os.mkdir(mphname)
                 dir_dict[mphb] = mphname
             print('Creating MPH L2 directory')
-            mph_dir = os.path.join(dir_dict['proj dir'],'L2MPH')
+            mph_dir = os.path.join(dir_dict['proj dir'], 'L2MPH')
             if not os.path.isdir(mph_dir):
                 os.mkdir(mph_dir)
             dir_dict['mph dir'] = mph_dir
@@ -136,12 +138,11 @@ def hindcast(params_filename):
         nbtot = len(xmlfs)
         c = 1
         for xmlf in xmlfs:
-            products = []
-            products.append(ProductIO.readProduct(xmlf))
+            products = [ProductIO.readProduct(xmlf)]
             myproduct = MyProduct(products, params, dir_dict['L1 dir'])
 
         # FOR S3 MAKE SURE THE NON-DEFAULT S3TBX SETTING IS SELECTED IN THE SNAP PREFERENCES!
-            if params['sensor'] == 'OLCI' and not 'PixelGeoCoding2' in str(myproduct.products[0].getSceneGeoCoding()):
+            if params['sensor'] == 'OLCI' and 'PixelGeoCoding2' not in str(myproduct.products[0].getSceneGeoCoding()):
                 print()
                 sys.exit('The S3 product was read without pixelwise geocoding, please check the preference settings of the S3TBX!')
 
