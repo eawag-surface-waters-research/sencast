93a88e60a00b00f47808d0130b592ca71af2b9cf
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-09 08:06:48 +0200

    - for olci, only look for images, which fully cover the perimeter

--- diasapis/coah_api.py
+++ diasapis/coah_api.py
@@ -18,7 +18,10 @@ download_address = api_endpoint + "/odata/v1/Products('{}')/$value"
 
 
 def get_download_requests(auth, wkt, start, end, sensor, resolution):
-    query = "instrumentshortname:{}+AND+producttype:{}+AND+beginPosition:[{}+TO+{}]+AND+footprint:\"Intersects({})\""
+    if sensor == "OLCI":
+        query = "instrumentshortname:{}+AND+producttype:{}+AND+beginPosition:[{}+TO+{}]+AND+footprint:\"Contains({})\""
+    else:
+        query = "instrumentshortname:{}+AND+producttype:{}+AND+beginPosition:[{}+TO+{}]+AND+footprint:\"Intersects({})\""
     datatype = get_dataset_id(sensor, resolution)
     query = query.format(sensor.lower(), datatype, start, end, wkt)
     uuids, product_names = search(auth, query)
