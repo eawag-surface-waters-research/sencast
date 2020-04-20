c2b7d37eeb471f23478a06171ccc49fa9f577594
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-20 12:23:14 +0200

    - use ancillary data for ozone and surf_press in c2rcc

--- processors/c2rcc/c2rcc.py
+++ processors/c2rcc/c2rcc.py
@@ -1,10 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import datetime
 import os
+import re
 import subprocess
 
+from polymer.ancillary_era5 import Ancillary_ERA5
+
 from packages.auxil import load_properties
+from packages.product_fun import get_lons_lats
 from packages.ql_mapping import plot_map
 
 # Key of the params section for this processor
@@ -18,7 +22,7 @@ QL_DIR = "L2C2RCC-{}"
 # A pattern for the name of the file to which the quicklooks will be saved (completed with product name and band name)
 QL_FILENAME = "L2C2RCC_{}_{}.png"
 # The name of the xml file for gpt
-GPT_XML_FILENAME = "c2rcc_{}.xml"
+GPT_XML_FILENAME = "c2rcc_{}_{}.xml"
 
 
 def process(env, params, l1_product_path, source_file, out_path):
@@ -29,6 +33,7 @@ def process(env, params, l1_product_path, source_file, out_path):
     sensor, resolution, wkt = params['General']['sensor'], params['General']['resolution'], params['General']['wkt']
     altnn, validexpression = params[PARAMS_SECTION]['altnn'], params[PARAMS_SECTION]['validexpression']
     vicar_properties_filename = params[PARAMS_SECTION]['vicar_properties_filename']
+    date_str = re.findall(r"\d{8}T\d{6}", product_name)[0]
 
     output_file = os.path.join(out_path, OUT_DIR, OUT_FILENAME.format(product_name))
     if os.path.isfile(output_file):
@@ -36,9 +41,9 @@ def process(env, params, l1_product_path, source_file, out_path):
         return output_file
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-    gpt_xml_file = os.path.join(out_path, GPT_XML_FILENAME.format(sensor.lower()))
+    gpt_xml_file = os.path.join(out_path, GPT_XML_FILENAME.format(sensor.lower(), date_str))
     if not os.path.isfile(gpt_xml_file):
-        rewrite_xml(gpt_xml_file, sensor, altnn, validexpression, vicar_properties_filename)
+        rewrite_xml(gpt_xml_file, date_str, sensor, altnn, validexpression, vicar_properties_filename, wkt)
 
     args = [gpt, gpt_xml_file, "-SsourceFile={}".format(source_file), "-PoutputFile={}".format(output_file)]
     if subprocess.call(args):
@@ -49,17 +54,24 @@ def process(env, params, l1_product_path, source_file, out_path):
     return output_file
 
 
-def rewrite_xml(gpt_xml_file, sensor, altnn, validexpression, vicar_properties_filename):
-    with open(os.path.join(os.path.dirname(__file__), GPT_XML_FILENAME.format(sensor.lower())), "r") as f:
+def rewrite_xml(gpt_xml_file, date_str, sensor, altnn, validexpression, vicar_properties_filename, wkt):
+    with open(os.path.join(os.path.dirname(__file__), GPT_XML_FILENAME.format(sensor.lower(), "")), "r") as f:
         xml = f.read()
 
     altnn_path = os.path.join(os.path.dirname(__file__), "altnn", altnn) if altnn else ""
 
+    ancillary = Ancillary_ERA5()
+    date = datetime.datetime.strptime(date_str, "%Y%m%dT%H%M%S")
+    lons, lats = get_lons_lats(wkt)
+    coords = (max(lats) + min(lats)) / 2, (max(lons) + min(lons)) / 2
+    ozone = round(ancillary.get("ozone", date)[coords])
+    surf_press = round(ancillary.get("surf_press", date)[coords])
+
     xml = xml.replace("${validPixelExpression}", validexpression)
     xml = xml.replace("${salinity}", str(0.05))
     xml = xml.replace("${temperature}", str(15.0))
-    xml = xml.replace("${ozone}", str(330.0))  # str(product_parameters.get('ozone'))
-    xml = xml.replace("${press}", str(1000.0))  # str(product_parameters.get('press'))
+    xml = xml.replace("${ozone}", str(ozone))
+    xml = xml.replace("${press}", str(surf_press))
     xml = xml.replace("${TSMfakBpart}", str(1.72))
     xml = xml.replace("${TSMfakBwit}", str(3.1))
     xml = xml.replace("${CHLexp}", str(1.04))
