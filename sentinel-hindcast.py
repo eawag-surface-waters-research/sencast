68072aade6c3a57f7e6626503e10734f92d4c8f8
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-22 11:33:10 +0200

    - beautified

--- sentinel-hindcast.py
+++ sentinel-hindcast.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 
 # Import libs, some of them might be unused here, but something magical happens when they are imported
-# which causes geos_c.dll and objectify.pyx errors to disappear.
-
+# which causes geos_c.dll and objectify.pyx errors to disappear in windows.
 import cartopy.crs
 import netCDF4
 
 from packages.main import hindcast
 
-hindcast("datalakes_sui_S3.ini")
+hindcast("parameters_wdoc_S3.ini")
