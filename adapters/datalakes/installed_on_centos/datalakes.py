63fdd4a0a3a01f2e3d3ef96afbc3dbec0b59eabd
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-27 19:20:58 +0200

    - load wkt in datalakes script

--- adapters/datalakes/installed_on_centos/datalakes.py
+++ adapters/datalakes/installed_on_centos/datalakes.py
@@ -6,11 +6,13 @@ import sys
 sys.path.append("/prj/sentinel-hindcast")
 
 from main import do_hindcast
-from auxil import load_environment, load_params
+from auxil import load_environment, load_params, load_wkt
 
 env, _ = load_environment()
 
 params, params_file = load_params("/prj/datalakes/datalakes_sui_S3.ini")
+if not params['General']['wkt']:
+    params['General']['wkt'], _ = load_wkt("{}.wkt".format(params['General']['wkt_name']), env['General']['wkt_path'])
 params['General']['start'] = params['General']['end']
 params['General']['end'] = datetime.datetime.now().strftime(r"%Y-%m-%dT%H:%M:%SZ")
 with open(params_file, "w") as f:
