eba743181497a6abf8f41a2af8b771a56a41b9c1
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-27 20:25:27 +0200

    - fix conda activate not working

--- adapters/datalakes/installed_on_centos/datalakes.sh
+++ adapters/datalakes/installed_on_centos/datalakes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /prj/datalakes/log
 filename=/prj/datalakes/log/$(date '+%Y-%m-%d-%H%M%S').log
-touch $filename
+touch "$filename"
+source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate sentinel-hindcast-37
-python datalakes.py > $filename
-# sudo shutdown
+python datalakes.py > "$filename" 2>&1
