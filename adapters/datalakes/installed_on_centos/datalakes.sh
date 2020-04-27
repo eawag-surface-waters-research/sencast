80a4a8bfa610af719ca9664b9c8043aadcab65bd
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-27 22:59:40 +0200

    - minor change

--- adapters/datalakes/installed_on_centos/datalakes.sh
+++ adapters/datalakes/installed_on_centos/datalakes.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 mkdir -p /prj/datalakes/log
-filename=/prj/datalakes/log/$(date '+%Y-%m-%d-%H%M%S').log
-touch "$filename"
 source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate sentinel-hindcast-37
-python datalakes.py > "$filename" 2>&1
+python /prj/datalakes/datalakes.py
