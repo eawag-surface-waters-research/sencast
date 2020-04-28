32f9316b4dca3662f786d5a81ec18ad27cea4519
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-28 18:57:33 +0200

    - moved script folder to project root - changed the way how datalakes runs

---
+++ scripts/datalakes.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+from datetime import datetime, timedelta
+
+sys.path.append("/prj/sentinel-hindcast")
+
+from auxil import load_params
+from main import hindcast
+
+params, params_file = load_params("/prj/datalakes/datalakes_sui_S3.ini")
+params['General']['start'] = params['General']['end']
+params['General']['end'] = (datetime.now() - timedelta(weeks=1)).strftime(r"%Y-%m-%dT%H:%M:%SZ")
+with open(params_file, "w") as f:
+    params.write(f)
+
+hindcast(params_file, max_parallel_downloads=1, max_parallel_processors=1, max_parallel_adapters=1)
