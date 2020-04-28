5c7236cddb8eb5ce16438157ad05e2d95532e18d
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-28 15:03:13 +0200

    - use datalakes scripts from sentinel-hindcast project directly instead of copying them to datalakes project

--- adapters/datalakes/scripts/datalakes.sh
+++ adapters/datalakes/scripts/datalakes.sh
@@ -2,4 +2,4 @@
 mkdir -p /prj/datalakes/log
 source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate sentinel-hindcast-37
-python /prj/sentinel-hindcast/datalakes/scripts/datalakes.py
+python /prj/sentinel-hindcast/adapters/datalakes/scripts/datalakes.py
