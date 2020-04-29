ccfb6303e052f2454ba01aecfd1bc8dfabf5f0f9
Rolf Scheuner <rolf.scheuner@eawag.ch>
2020-04-29 15:05:45 +0200

    - another try for better log...

--- scripts/datalakes.sh
+++ scripts/datalakes.sh
@@ -2,4 +2,4 @@
 mkdir -p "/prj/datalakes/log"
 source /opt/anaconda3/etc/profile.d/conda.sh
 conda activate sentinel-hindcast-37
-python /prj/sentinel-hindcast/scripts/datalakes.py |& tee "/prj/datalakes/log/$(date '+%Y-%m-%d-%H%M%S').log"
+python /prj/sentinel-hindcast/scripts/datalakes.py &> "/prj/datalakes/log/$(date '+%Y-%m-%d-%H%M%S').log"
