b88c101a89196d95d59f687da48ce4cf5f42d35f
James Runnalls <james.runnalls@eawag.ch>
2020-05-19 09:35:27 +0200

    Implemented Creodias API

--- externalapis/creodias_api.py
+++ externalapis/creodias_api.py
@@ -11,22 +11,51 @@ from zipfile import ZipFile
 # Documentation for CREODIAS API can be found here:
 # https://creodias.eu/eo-data-finder-api-manual
 
-# API endpoint address
-api_endpoint = "https://finder.creodias.eu/resto/api"
-
 # search address
-search_address = api_endpoint + "/collections/Sentinel3/search.json?"
+search_address = "https://finder.creodias.eu/resto/api/collections/Sentinel3/search.json?{}"
 
 # download address
-download_address = api_endpoint + ""
+download_address = "https://zipper.creodias.eu/download/{}?token={}"
+
+# token address
+token_address = 'https://auth.creodias.eu/auth/realms/DIAS/protocol/openid-connect/token'
 
 
-def get_download_requests(auth, start, end, sensor, resolution, wkt):
-    query = "maxRecords={}&startDate={}&completionDate={}&instrument={}&productType={}&status={}&geometry={}&dataset={}"
-    datatype = get_dataset_id(sensor, resolution)
-    query = query.format(sensor.lower(), datatype, start, end, wkt)
-    uuids, product_names = search(auth, query)
+def get_download_requests(auth, startDate, completionDate, sensor, resolution, wkt):
+    query = "maxRecords={}&startDate={}&completionDate={}&instrument={}&geometry={}&productType={}"
+    maxRecords = 100
+    geometry = wkt.replace(" ", "", 1).replace(" ", "+")
+    instrument, productType = get_dataset_id(sensor, resolution)
+    query = query.format(maxRecords, startDate, completionDate, instrument, geometry, productType)
+    uuids, product_names, timelinesss, beginpositions, endpositions = search(query)
+    uuids, product_names = timeliness_filter(uuids, product_names, timelinesss, beginpositions, endpositions)
     return [{'uuid': uuid} for uuid in uuids], product_names
+
+def timeliness_filter(uuids, product_names, timelinesss, beginpositions, endpositions):
+    num_products = len(uuids)
+    uuids_filtered, product_names_filtered, positions, timelinesss_filtered = [], [], [], []
+    for i in range(num_products):
+        curr_pos = (beginpositions[i], endpositions[i])
+        if curr_pos in positions:
+            curr_proj_idx = positions.index(curr_pos)
+            if timelinesss[i] == 'Non Time Critical' and timelinesss_filtered[curr_proj_idx] == 'Near Real Time':
+                timelinesss_filtered[curr_proj_idx] = timelinesss[i]
+                uuids_filtered[curr_proj_idx] = uuids[i]
+                product_names_filtered[curr_proj_idx] = product_names[i]
+                positions[curr_proj_idx] = (beginpositions[i], endpositions[i])
+            elif timelinesss[i] == 'Near Real Time' and timelinesss_filtered[curr_proj_idx] == 'Non Time Critical':
+                continue
+            else:
+                timelinesss_filtered.append(timelinesss[i])
+                uuids_filtered.append(uuids[i])
+                product_names_filtered.append(product_names[i])
+                positions.append((beginpositions[i], endpositions[i]))
+        else:
+            timelinesss_filtered.append(timelinesss[i])
+            uuids_filtered.append(uuids[i])
+            product_names_filtered.append(product_names[i])
+            positions.append((beginpositions[i], endpositions[i]))
+    return uuids_filtered, product_names_filtered
 
 
 def do_download(auth, download_request, product_path):
@@ -35,43 +64,38 @@ def do_download(auth, download_request, product_path):
 
 def get_dataset_id(sensor, resolution):
     if sensor == 'OLCI' and int(resolution) < 1000:
-        return 'OL_1_EFR___'
+        return 'OL', 'EFR'
     elif sensor == 'OLCI' and int(resolution) >= 1000:
-        return 'OL_1_ERR___'
-    elif sensor == 'MSI':
-        return 'S2MSI1C'
+        return 'OL', 'ERR'
     else:
-        raise RuntimeError("COAH API is not yet implemented for sensor: {}".format(sensor))
+        raise RuntimeError("CREODIAS API is not yet implemented for sensor: {}".format(sensor))
 
 
-def search(auth, query):
+def search(query):
     print("Search for products: {}".format(query))
     uuids, filenames = [], []
-    start, rows = 0, 100
+    timelinesss, beginpositions, endpositions = [], [], []
     while True:
-        response = requests.get(search_address.format(query, start, rows), auth=auth)
+        response = requests.get(search_address.format(query))
         if response.status_code == codes.OK:
-            root = ElementTree.fromstring(response.text)
-            for entry in root.findall(prepend_ns("entry")):
-                for str_property in entry.findall(prepend_ns("str")):
-                    if str_property.attrib['name'] == "uuid":
-                        uuids.append(str_property.text)
-                    elif str_property.attrib['name'] == "filename":
-                        filenames.append(str_property.text)
-            has_next_page = False
-            for link in root.findall(prepend_ns('link')):
-                if link.attrib['rel'] == "next":
-                    start = start + rows
-                    has_next_page = True
-            if not has_next_page:
-                return uuids, filenames
+            root = response.json()
+            for feature in root['features']:
+                uuids.append(feature['id'])
+                filenames.append(feature['properties']['title'])
+                timelinesss.append(feature['properties']['timeliness'])
+                beginpositions.append(feature['properties']['startDate'])
+                endpositions.append(feature['properties']['completionDate'])
+            return uuids, filenames, timelinesss, beginpositions, endpositions
         else:
-            raise RuntimeError("Unexpeted response: {}".format(response.text))
+            raise RuntimeError("Unexpected response: {}".format(response.text))
 
 
 def download(auth, uuid, filename):
+    username = auth[0]
+    password = auth[1]
+    token = get_token(username, password)
     os.makedirs(os.path.dirname(filename), exist_ok=True)
-    response = requests.get(download_address.format(uuid), auth=auth, stream=True)
+    response = requests.get(download_address.format(uuid, token), stream=True)
     if response.status_code == codes.OK:
         with open(filename + '.zip', 'wb') as down_stream:
             for chunk in response.iter_content(chunk_size=65536):
@@ -83,9 +107,15 @@ def download(auth, uuid, filename):
         print("Unexpected response on download request: {}".format(response.text))
 
 
-def prepend_ns(s):
-    return '{http://www.w3.org/2005/Atom}' + s
-
-
-def prepend_os(s):
-    return '{http://a9.com/-/spec/opensearch/1.1/}' + s
+def get_token(username, password):
+    token_data = {
+        'client_id': 'CLOUDFERRO_PUBLIC',
+        'username': username,
+        'password': password,
+        'grant_type': 'password'
+    }
+    response = requests.post(token_address, data=token_data).json()
+    try:
+        return response['access_token']
+    except KeyError:
+        raise RuntimeError(f'Unable to get token. Response was {response}')
