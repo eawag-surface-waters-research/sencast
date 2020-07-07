de07a361e3cfc6092a7120eafb3f200a18766df8
James Runnalls <james.runnalls@eawag.ch>
2020-07-07 13:06:54 +0200

    Remove corrupted files

--- processors/idepix/idepix.py
+++ processors/idepix/idepix.py
@@ -41,6 +41,10 @@ def process(env, params, l1product_path, _, out_path):
     args = [gpt, gpt_xml_file, "-c", env['General']['gpt_cache_size'], "-e", "-SsourceFile={}".format(l1product_path),
             "-PoutputFile={}".format(output_file)]
     if subprocess.call(args):
+        if os.path.exists(output_file):
+            os.remove(output_file)
+        else:
+            print("No file was created.")
         raise RuntimeError("GPT Failed.")
 
     return output_file
