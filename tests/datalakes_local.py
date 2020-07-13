f737e75d519ee437f90f4dfd797d8985f1bf9bfe
James Runnalls <james.runnalls@eawag.ch>
2020-07-13 09:59:11 +0200

    Remove environement and parameter files

---
+++ tests/datalakes_local.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+sys.path.append("/home/jamesrunnalls/eawagrs/sentinel-hindcast/build/sentinel-hindcast")
+
+from auxil import load_params
+from main import hindcast
+
+params, params_file = load_params("/home/jamesrunnalls/eawagrs/sentinel-hindcast/build/sentinel-hindcast/parameters/parameters_wdoc_S3.ini")
+#params['General']['start'] = params['General']['end']
+#params['General']['end'] = (date.today() - timedelta(days=5)).strftime(r"%Y-%m-%dT%H:%M:%SZ")
+with open(params_file, "w") as f:
+    params.write(f)
+
+hindcast(params_file, max_parallel_downloads=1, max_parallel_processors=1, max_parallel_adapters=1)
