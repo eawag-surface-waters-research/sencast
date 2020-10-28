9ca9b0cf43a58a2b3bb1b218fc53f9b0c9150d0c
James Runnalls <james.runnalls@eawag.ch>
2020-10-28 14:37:42 +0100

    Find files

--- tests/extractpixels.py
+++ tests/extractpixels.py
@@ -1,9 +1,31 @@
 from postprocess.pixelextraction.pixelextration import pixelextration
+import os
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories
+    # names in the given directory
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+
+    return allFiles
+
+
+def filterFiles(files, string):
+    return [file for file in files if string in file]
+
 
 folder = "/media/jamesrunnalls/JamesSSD/Eawag/EawagRS/Sencast"
 coords = [{"name": "A", "lat": 46.44829, "lng": 6.561426}]
-files = ["/media/jamesrunnalls/JamesSSD/Eawag/EawagRS/Sencast/build/DIAS/output_data/datalakes_sui_S3_sui_2018-06-01_2018-06-07/L2PP/L2PP_L2SD_L2POLY_L1P_reproj_ERA5_S3A_OL_1_EFR____20180601T102022_20180601T102322_20180602T152636_0180_032_008_2160_LN1_O_NT_002.SEN3.nc",
-          "/media/jamesrunnalls/JamesSSD/Eawag/EawagRS/Sencast/build/DIAS/output_data/datalakes_sui_S3_sui_2018-06-01_2018-06-07/L2PP/L2PP_L2SD_L2POLY_L1P_reproj_ERA5_S3A_OL_1_EFR____20180602T095411_20180602T095711_20180603T144256_0180_032_022_2160_LN1_O_NT_002.SEN3.nc",
-          "/media/jamesrunnalls/JamesSSD/Eawag/EawagRS/Sencast/build/DIAS/output_data/datalakes_sui_S3_sui_2018-06-01_2018-06-07/L2PP/L2PP_L2SD_L2POLY_L1P_reproj_ERA5_S3A_OL_1_EFR____20180603T092800_20180603T093100_20180604T140652_0179_032_036_2160_LN1_O_NT_002.SEN3.nc"]
+files = getListOfFiles("/media/jamesrunnalls/JamesSSD/Eawag/EawagRS/Sencast/build/DIAS/output_data/datalakes_sui_S3_sui_2018-06-01_2018-06-07")
+files = filterFiles(files, "L2PP_")
 
 pixelextration(files, coords, folder)
