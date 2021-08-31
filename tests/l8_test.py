5c8b8f3165469fc5f192e23a55b7cd09fa31b896
rolf.scheuner@eawag.ch <rolf.scheuner@eawag.ch>
2021-08-31 11:26:58 +0200

    removed unused test scripts, added c2rcc for OLI_TIRS

--- tests/l8_test.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-
-from ..main import hindcast
-
-
-hindcast(os.path.join(os.path.dirname(os.path.abspath(__file__)), "parameters", "test_L8.ini"))
