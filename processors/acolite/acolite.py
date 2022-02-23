00f0c94f0a4311b48df27945d0f6ee70e9f19f47
odermatt <Anno_2014>
2022-02-23 15:24:15 +0100

    qlrgb.py: - added missing import statement for log, which didn't work on my Mac -

--- processors/acolite/acolite.py
+++ processors/acolite/acolite.py
@@ -37,6 +37,8 @@ def process(env, params, l1product_path, _, out_path):
     l2w_mask_smooth = params[PARAMS_SECTION]['l2w_mask_smooth']
     l2w_mask_cirrus_threshold = params[PARAMS_SECTION]['l2w_mask_cirrus_threshold']
     l2w_mask_negative_rhow = params[PARAMS_SECTION]['l2w_mask_negative_rhow']
+    dsf_write_aot_550 = params[PARAMS_SECTION]['dsf_write_aot_550']
+    dsf_write_tiled_parameters = params[PARAMS_SECTION]['dsf_write_tiled_parameters']
     l2w_parameters = params[PARAMS_SECTION]['l2w_parameters']
     lons, lats = get_lons_lats(wkt)
     limit = "{},{},{},{}".format(min(lats), min(lons), max(lats), max(lons))
@@ -52,7 +54,9 @@ def process(env, params, l1product_path, _, out_path):
 
     settings_file = os.path.join(out_path, REPROD_DIR, SETTINGS_FILENAME.format(sensor))
     if not os.path.isfile(settings_file):
-        rewrite_settings_file(settings_file, sensor, resolution, limit, l2w_mask_wave, l2w_mask_threshold, l2w_mask_smooth, l2w_mask_cirrus_threshold, l2w_mask_negative_rhow, l2w_parameters)
+        rewrite_settings_file(settings_file, sensor, resolution, limit, l2w_mask_wave, l2w_mask_threshold,
+                              l2w_mask_smooth, l2w_mask_cirrus_threshold, l2w_mask_negative_rhow, dsf_write_aot_550,
+                              dsf_write_tiled_parameters, l2w_parameters)
 
     tmp_path = os.path.join(out_path, "tmp")
     ac.acolite_run(settings_file, l1product_path, tmp_path)
@@ -75,7 +79,9 @@ def process(env, params, l1product_path, _, out_path):
     return out_file
 
 
-def rewrite_settings_file(settings_file, sensor, resolution, limit, l2w_mask_wave, l2w_mask_threshold, l2w_mask_smooth, l2w_mask_cirrus_threshold, l2w_mask_negative_rhow, l2w_parameters):
+def rewrite_settings_file(settings_file, sensor, resolution, limit, l2w_mask_wave, l2w_mask_threshold,
+                          l2w_mask_smooth, l2w_mask_cirrus_threshold, l2w_mask_negative_rhow, dsf_write_aot_550,
+                          dsf_write_tiled_parameters, l2w_parameters):
     with open(os.path.join(os.path.dirname(__file__), SETTINGS_FILENAME.format(sensor)), "r") as f:
         text = f.read()
 
@@ -86,6 +92,8 @@ def rewrite_settings_file(settings_file, sensor, resolution, limit, l2w_mask_wav
     text = text.replace("${l2w_mask_smooth}", l2w_mask_smooth)
     text = text.replace("${l2w_mask_cirrus_threshold}", l2w_mask_cirrus_threshold)
     text = text.replace("${l2w_mask_negative_rhow}", l2w_mask_negative_rhow)
+    text = text.replace("${dsf_write_aot_550}", dsf_write_aot_550)
+    text = text.replace("${dsf_write_tiled_parameters}", dsf_write_tiled_parameters)
     text = text.replace("${l2w_parameters}", l2w_parameters)
 
     os.makedirs(os.path.dirname(settings_file), exist_ok=True)
