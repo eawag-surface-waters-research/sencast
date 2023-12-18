8195fc826447f9a07cae1f5b84ab9ecb438ce5b2
James Runnalls <runnalls.james@gmail.com>
2023-12-18 10:13:24 +0100

    Add option to select date range

--- examples/looping.py
+++ examples/looping.py
@@ -8,6 +8,7 @@ import netCDF4
 import cartopy.crs as ccrs
 import sys
 import configparser
+from datetime import datetime, timedelta
 sys.path.append("/sencast")
 
 from main import sencast
@@ -17,18 +18,25 @@ ini_file = '../parameters/test_S3_processors.ini'
 wkts = ["greifen", "garda"]
 dates = ["2022-05-21", "2022-05-22"]
 
+# start_date = datetime(2023, 1, 1)
+# end_date = datetime(2023, 12, 31)
+# dates = [(start_date + timedelta(days=x)).strftime("%Y-%m-%d") for x in range((end_date - start_date).days + 1)]
+
 config = configparser.ConfigParser()
 config.read(ini_file)
 
 for date in dates:
     for wkt in wkts:
-        print("Running Sencast for {} on {}".format(wkt, date))
-        config["General"]["wkt_name"] = wkt
-        config["General"]["start"] = "{}T00:00:00.000Z".format(date)
-        config["General"]["end"] = "{}T23:59:59.999Z".format(date)
+        try:
+            print("Running Sencast for {} on {}".format(wkt, date))
+            config["General"]["wkt_name"] = wkt
+            config["General"]["start"] = "{}T00:00:00.000Z".format(date)
+            config["General"]["end"] = "{}T23:59:59.999Z".format(date)
 
-        with open(ini_file, 'w') as configfile:
-            config.write(configfile)
+            with open(ini_file, 'w') as configfile:
+                config.write(configfile)
 
-        sencast(ini_file)
+            sencast(ini_file)
+        except Exception as e:
+            print(e)
 
