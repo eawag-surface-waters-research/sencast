6d04d89586f23603e2d582663937a7fa3990cd51
James Runnalls <runnalls.james@gmail.com>
2024-12-20 12:31:08 +0100

    Remove examples

--- examples/temp.py
+++
@@ -1,9 +0,0 @@
-import rioxarray # for the extension to load
-import xarray
-import rasterio
-
-xds = xarray.open_dataset("/home/runnalja/Desktop/DIAS/output_data/test_Collection_processors_geneva_2022-05-21_2022-05-21/L2C/L2C_LC09_L2SP_195028_20220521_20230416_02_T1.nc", decode_coords="all")
-xds = xds.rio.set_spatial_dims(x_dim='lon', y_dim='lat')
-xds = xds.rio.write_crs("EPSG:32632", inplace=True)
-xds_lonlat = xds.rio.reproject("EPSG:4326")
-print(xds_lonlat)
--- examples/test.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-s3 = boto3.resource('s3',
-                    aws_access_key_id="",
-                    aws_secret_access_key="",
-                    endpoint_url="https://eodata.cloudferro.com", )
-bucket = s3.Bucket("DIAS")
-prefix = "Sentinel-2/MSI/L1C/2023/06/10/S2B_MSIL1C_20230610T101609_N0509_R065_T32TLS_20230610T122126.SAFE"
-print(list(bucket.objects.filter(Prefix=prefix)))
